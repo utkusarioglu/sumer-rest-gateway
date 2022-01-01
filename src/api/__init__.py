032d01b8dd576f4b6913c5489f6f692e5c1f05a3
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-01-01 10:56:28 +0000

    Remove reduntant empty lines

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -2,7 +2,6 @@ from flask import jsonify
 from flask_restful import Api, Resource
 import config
 
-
 class MessageApi(Resource):
   def get(self):
     return jsonify("hello man")
@@ -10,4 +9,3 @@ class MessageApi(Resource):
 def crete_apis(app):
   api = Api(app, prefix=config.API_PREFIX)
   api.add_resource(MessageApi, "/message")
-
